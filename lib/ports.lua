18727f2104c60e15fef0962d396edc14bad2b6d9
Nick Rothwell <nick@cassiel.com>
2025-08-25 16:23:52 +0100

    Refactoring.

---
+++ lib/ports.lua
@@ -0,0 +1,99 @@
+local function setup_midi(G, callbacks)
+    --[[
+        Set up MIDI endpoints via virtual ports (so the devices
+        here might not actually be connected, and/or a script
+        reload might be needed).
+    ]]
+
+    -- Arrays indexed by vport:
+    local devices = { }
+    local names = { }
+    
+    for i = 1, #midi.vports do
+        devices[i] = midi.connect(i)
+        -- The trim is mainly for the parameter page. (Perhaps we should
+        -- have a second table with longer names for the script page.)
+        table.insert(
+            names,
+            "port "..i..": "..util.trim_string_to_width(devices[i].name, 40)
+        )
+        
+        -- I'm a little unsure of how G is scoped here, but it seems to work:
+        
+        devices[i].event =
+            function (x)
+                print("PORT [" .. i .. "]")
+                if i == G.midi.mf_target then
+                    local msg = midi.to_msg(x)
+                    callbacks.process_note(msg.note, (msg.type == "note_on"))
+                end
+            
+                tab.print(midi.to_msg(x))
+            end
+    end
+    
+    params:add_option("mf", "MIDI Fighter", names, 1)
+    params:set_action("mf", function(x) G.midi.mf_target = x end)
+    
+    params:add_option("daw", "DAW", names, 2)
+    params:set_action("daw", function(x) G.midi.daw_target = x end)
+    
+    G.midi = {
+        devices = devices,
+        names = names,
+        mf_target = 1,          -- Hope that fires before the param callback?
+        daw_target = 2
+    }
+end
+
+local function setup_arcs(G)
+    --[[
+        We are unlikely to connect more than one arc, but let's follow
+        the MIDI template, connecting to all ports and
+        and having a menu selection param for the one we're using.
+    ]]
+
+    -- Arrays indexed by vport:
+    local devices = { }
+    local names = { }
+    
+    for i = 1, #arc.vports do
+        devices[i] = arc.connect(i)
+        -- The trim is mainly for the parameter page. (Perhaps we should
+        -- have a second table with longer names for the script page.)
+        table.insert(
+            names,
+            "port "..i..": "..util.trim_string_to_width(devices[i].name, 40)
+        )
+        
+        devices[i].delta =
+            function (n, d)
+                print("port " .. i .. " enc " .. n .. " delta " .. d)
+            end
+            
+        devices[i].key =
+            function (n, z)
+                print("port " .. i .. " enc " .. n .. " key " .. z)
+            end
+    end
+    
+    params:add_option("arc", "arc 4", names, 1)
+    params:set_action("arc", function(x) G.arcs.target = x end)
+    
+    G.arcs = {
+        devices = devices,
+        names = names,
+        target = 1
+    }
+end
+
+local function setup(G, callbacks)
+    tab.print(callbacks)
+    params:add_separator("The Printer Jam: Ports")
+    setup_midi(G, callbacks)
+    setup_arcs(G)
+end
+
+return {
+    setup = setup
+}
--- printer-jam-norns.lua
+++ printer-jam-norns.lua
@@ -1,7 +1,7 @@
 -- ## printer-jam-norns
 -- Driver for MIDI Fighter and arc 4
 -- to generate controller bindings.
--- NOTE: will require _shado_.
+-- NOTE: requires _shado_.
 --
 -- Nick Rothwell, nick@cassiel.com.
 
@@ -12,10 +12,16 @@
 
     Input: one MIDI Fighter Spectra, one monome arc 4. There are six banks of
     four controller outputs, hence 72 outputs. The rest of the Spectra
-    is relatively ad-hoc note output (or that might even be directly mapped
-    from Spectra to DAW, in which case we need to echo it).
+    is relatively ad-hoc note output.
+    
+    We assume the Spectra is on Bank 1, default MIDI output. We will
+    probably also get note input from the side buttons, which we ignore.
     
     Output: feedback to the Spectra and arc, and outward MIDI to DAW.
+    
+    Implementation: we rig up a custom shado renderer which drives
+    the Spectra at three levels (full white, half white, off).
+    Scheme to be used for colour rendering TBC.
 ]]
 
 --[[
@@ -30,14 +36,45 @@
     - Third row, 3+4: scene select. Also: something is fudging
       a C-1 for scene launch on chording. (Max, with 100ms delay.)
     - Remaining buttons: 6 stacks of 8 controllers. Current in
-      low red, "shift" selection (held) bright red.
+      low red, "shift" selection (held) bright red. We could also
+      add some eye candy here when the arc does something.
 ]]
 
--- All globals:
+-- Development: purge lib on reload:
+
+for k, _ in pairs(package.loaded) do
+    if k:find("printer-jam-norns.", 1, true) == 1 then
+        print("purge " .. k)
+        package.loaded[k] = nil
+    end
+end
+
+local ports = require "printer-jam-norns.lib.ports"
+
+-- All state globals:
 G = { }
 
 BASE_PITCH = 36
 NUM_BANKS = 6       -- Number of controller banks (currently 6)
+
+-- Colour table: e.g. COLOURS.yellow.lo
+
+do
+    local names = {
+        "red", "orange", "yellow", "lime",
+        "green", "cyan", "blue", "purple",
+        "pink", "white"
+    }
+
+    COLOURS = { }
+    
+    for i = 1, #names do
+        local v_lo = (i - 1) * 12 + 13
+        local v_hi = (i - 1) * 12 + 7
+        if v_lo > 120 then v_lo = 1 end
+        COLOURS[names[i]] = {lo = v_lo, hi = v_hi}
+    end
+end
 
 local function underside(how)
     local mf = G.midi.devices[G.midi.mf_target]
@@ -58,121 +95,36 @@ end
 ]]
 
 local function process_note(pitch, is_on)
+    -- print("NOTE " .. pitch .. " mode " .. (is_on and "ON" or "OFF"))
+        
     G.num_held_notes = (G.num_held_notes or 0) + (is_on and 1 or -1)
     
     local daw = G.midi.devices[G.midi.daw_target]
+    local button0 = pitch - BASE_PITCH
     
-    --[[
-        For all except the first N buttons, we pass through the note
-        event, but on MIDI channel 1.
-    ]]
+    if button0 >= 0 and button0 < 16 then   -- Protection against out-of-range notes.
+                                            -- (We should also filter on MIDI channel.)
+        --[[
+            Nudge controls, looper controls. Just echo the notes
+            through (with fudged velocity and channel) before we
+            do any LED driving.
+        ]]
 
-    if pitch >= BASE_PITCH + NUM_BANKS then
-        -- MIDI Fighter can't do velocity, so just default it:
-        if is_on then
-            daw:note_on(pitch, 64, 1)
-        else
-            daw:note_off(pitch, 64, 1)
-        end
-    end
-    
-    print("NOTE " .. pitch .. " mode " .. (is_on and "ON" or "OFF"))
-    
-    underside(G.num_held_notes > 0)
-end
-
-local function setup_midi()
-    --[[
-        Set up MIDI endpoints via virtual ports (so the devices
-        here might not actually be connected, and/or a script
-        reload might be needed).
-    ]]
-
-    -- Arrays indexed by vport:
-    local devices = { }
-    local names = { }
-    
-    for i = 1, #midi.vports do
-        devices[i] = midi.connect(i)
-        -- The trim is mainly for the parameter page. (Perhaps we should
-        -- have a second table with longer names for the script page.)
-        table.insert(
-            names,
-            "port "..i..": "..util.trim_string_to_width(devices[i].name, 40)
-        )
-        
-        devices[i].event =
-            function (x)
-                print("PORT [" .. i .. "]")
-                if i == G.midi.mf_target then
-                    local msg = midi.to_msg(x)
-                    process_note(msg.note, (msg.type == "note_on"))
-                end
-            
-                tab.print(midi.to_msg(x))
+        if button0 >= NUM_BANKS then
+            -- MIDI Fighter can't do velocity, so just default it:
+            if is_on then
+                daw:note_on(pitch, 64, 1)
+            else
+                daw:note_off(pitch, 64, 1)
             end
+        end        
+    
+        underside(G.num_held_notes > 0)
     end
-    
-    params:add_option("mf", "MIDI Fighter", names, 1)
-    params:set_action("mf", function(x) G.midi.mf_target = x end)
-    
-    params:add_option("daw", "DAW", names, 2)
-    params:set_action("daw", function(x) G.midi.daw_target = x end)
-    
-    G.midi = {
-        devices = devices,
-        names = names,
-        mf_target = 1,          -- Hope that fires before the param callback?
-        daw_target = 2
-    }
-end
-
-local function setup_arcs()
-    --[[
-        We are unlikely to connect more than one arc, but let's follow
-        the MIDI template, connecting to all ports and
-        and having a menu selection param for the one we're using.
-    ]]
-
-    -- Arrays indexed by vport:
-    local devices = { }
-    local names = { }
-    
-    for i = 1, #arc.vports do
-        devices[i] = arc.connect(i)
-        -- The trim is mainly for the parameter page. (Perhaps we should
-        -- have a second table with longer names for the script page.)
-        table.insert(
-            names,
-            "port "..i..": "..util.trim_string_to_width(devices[i].name, 40)
-        )
-        
-        devices[i].delta =
-            function (n, d)
-                print("port " .. i .. " enc " .. n .. " delta " .. d)
-            end
-            
-        devices[i].key =
-            function (n, z)
-                print("port " .. i .. " enc " .. n .. " key " .. z)
-            end
-    end
-    
-    params:add_option("arc", "arc 4", names, 1)
-    params:set_action("arc", function(x) G.arcs.target = x end)
-    
-    G.arcs = {
-        devices = devices,
-        names = names,
-        target = 1
-    }
-
 end
 
 function init()
-    params:add_separator("The Printer Jam")
-    setup_midi()
-    setup_arcs()
+    ports.setup(G, {process_note = process_note})
 end
 
 function redraw()
